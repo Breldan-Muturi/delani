687277f2a2b4f93ec687a52a491bad1431d1ada2
Turi-byte <breldanmuturi001@gmail.com>
2019-07-19 21:08:13 +0300

    Add hover function to our portfolio images

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,28 @@ $(document).ready(function() {
       $(".product-description").toggle('slow');
       $(".product").toggle('slow');
     });
+    $("#work4").hover(function(){
+        $(".work4").toggle('slow');
+    });
+    $("#work3").hover(function(){
+      $(".work3").toggle('slow');
+    });
+    $("#work2").hover(function(){
+      $(".work2").toggle('slow');
+    });
+    $("#work1").hover(function(){
+      $(".work1").toggle('slow');
+    });
+    $("#work5").hover(function(){
+      $(".work5").toggle('slow');
+    });
+    $("#work6").hover(function(){
+      $(".work6").toggle('slow');
+    });
+    $("#work7").hover(function(){
+      $(".work7").toggle('slow');
+    });
+    $("#work8").hover(function(){
+      $(".work8").toggle('slow');
+    });
   });
