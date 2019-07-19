f5657d9274fb6350f9b2e465270915cb43827ff8
Turi-byte <breldanmuturi001@gmail.com>
2019-07-19 15:56:55 +0300

    Centre align the headers for 'what we do' and allows toggle function when the paragraph is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-    $("h3, #design_icon").click(function() {
+    $(".design-p, #design_icon").click(function() {
       $(".design-description").toggle('slow');
       $(".design").toggle('slow');
     });
-    $("h3, #dev_icon").click(function() {
+    $(".dev-p, #dev_icon").click(function() {
         $(".dev-description").toggle('slow');
         $(".dev").toggle('slow');
     });
-    $("h3,#product_icon").click(function() {
+    $(".product-p,#product_icon").click(function() {
       $(".product-description").toggle('slow');
       $(".product").toggle('slow');
     });
